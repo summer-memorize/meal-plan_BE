9ff23f433a60778cd93a20379092acd81f15c839
Jiyu Jang <apryll7995@gmail.com>
2023-10-22 17:19:45 +0900

    mealPlans router에 post, put, delete 추가

--- src/mealPlans/mealPlans.route.ts
+++ src/mealPlans/mealPlans.route.ts
@@ -6,5 +6,8 @@ const mealPlanService = new MealPlanService();
 
 // [GET] /mealplans?year=2023
 router.get("/", mealPlanService.getMealPlans);
+router.post("/", mealPlanService.createMealPlan);
+router.put("/", mealPlanService.updateMealPlan);
+router.delete("/", mealPlanService.deleteMealPlan);
 
 export default router;
--- src/mealPlans/mealPlans.service.ts
+++ src/mealPlans/mealPlans.service.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import MealPlan from "./mealPlans.schema";
+import { CustomError } from "../utils/customError";
 
 export class MealPlanService {
   public getMealPlans = async (req: Request, res: Response, next: NextFunction) => {
@@ -8,6 +9,47 @@ export class MealPlanService {
 
       const mealPlans = await MealPlan.find({ date: { $regex: `${year}-[0-9]{2}-[0-9]{2}` } });
       res.status(200).json({ data: mealPlans });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public createMealPlan = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // 이미 존재하는 날짜인지 확인
+      const isExist = await MealPlan.find({ date: req.body.date });
+      if (isExist.length > 0) throw new CustomError("CONFLICT");
+
+      const mealPlan = new MealPlan(req.body);
+      await mealPlan.save();
+
+      res.status(201).send("created");
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public updateMealPlan = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const mealPlan = req.body;
+      const updatedMealPlan = await MealPlan.findOneAndUpdate({ date: mealPlan.date }, mealPlan, { new: false });
+
+      if (!updatedMealPlan) throw new CustomError("NOT_FOUND");
+
+      res.status(200).send("ok");
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public deleteMealPlan = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { date } = req.body;
+      const deletedMealPlan = await MealPlan.findOneAndDelete({ date });
+
+      if (!deletedMealPlan) throw new CustomError("NOT_FOUND");
+
+      res.status(200).send("ok");
     } catch (err) {
       next(err);
     }
