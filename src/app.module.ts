776b2209f5514c89d9d181fec578acd26fe6b91e
Jiyu Jang <apryll7995@gmail.com>
2023-11-09 18:34:33 +0900

    logger middleware 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MealplanModule } from './mealplan/mealplan.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [MealplanModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
